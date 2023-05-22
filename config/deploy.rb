bf3ed38b8cf4ba60a4e86caa308ca219d0ad598c
Dhaval G <dhavalg.spaceo@gmail.com>
2023-05-22 16:10:13 +0530

    - Add application yml file in shared linked files.

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :branch, 'staging'
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", 'config/master.key'
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/master.key')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/master.key', 'config/application.yml')
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
